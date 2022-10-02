df3e535f3c6ebb2bedc25f3127a2d712e57ded2f
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2022-10-02 13:30:58 +0200

    Update 3_ricorsione.cpp

--- 3_ricorsione.cpp
+++ 3_ricorsione.cpp
@@ -2,6 +2,7 @@
 //this lets you insert a number and choose whether you want to calculate its factorial or its Collatz sequence (Cfr: Collatz Conjecture). 
 
 #include <iostream>
+#include <cmath>
 using namespace std; 
 
 int fattoriale(int n){
@@ -25,6 +26,15 @@ int collatz(int numb){
     }
 }
 
+bool isItPrime(int num){                    // <-- make it recursive!
+    for(int i = 2; i <= sqrt(num); i++){
+        if(num%i==0){
+            return false; 
+        }
+    }
+    return true; 
+}
+
 int main(){
     int num = -1; 
     
@@ -35,9 +45,10 @@ int main(){
 
     short int choice = -1; 
 
-    while(choice!=1 && choice !=2){
+    while(choice!=1 && choice !=2 && choice!=3){
         cout << "1) Evaluate "<<num<< "'s factorial number."<<endl;
         cout << "2) Evaluate "<<num<<"'s Collatz sequence."<<endl; 
+        cout << "3) Evaluate if "<<num<< " is a prime number."<<endl; 
         cin >> choice;
 
         if(choice == 1){
@@ -45,6 +56,13 @@ int main(){
         }else if(choice == 2){
             cout << "Evaluating its Collatz sequence. . ."<<endl; 
             cout << collatz(num); 
+        }else if(choice == 3){
+            bool prime = isItPrime(num); 
+            if(prime){
+                cout << num << " is a prime number."<<endl; 
+            }else{
+                cout << num << " is not a prime number."<<endl; 
+            }
         }else{
             cout <<endl << "Please insert a valid number."<<endl<<endl; 
         }
