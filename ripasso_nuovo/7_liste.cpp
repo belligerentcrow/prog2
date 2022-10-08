09506191ab0d5090b86fb806035c142103dfb98c
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2022-10-08 13:42:53 +0200

    liste

---
+++ ripasso_nuovo/7_liste.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std; 
+
+template <typename T>
+class Node {
+    public: 
+        T data; 
+        Node * succ; 
+};
+
+template <typename T>
+class SList{
+    private: 
+        Node * head; 
+    public: 
+        SList(){head = nullptr;}
+        ~SList();
+        Node* getHead(){
+            return head; 
+        }
+        
+        void insert(T val){
+            Node * newNode = new Node; 
+            newNode->data = val; 
+            newNode->succ = this->head; 
+            this->head = newNode; 
+        }
+
+        void insert(Node * prev, T val){
+            Node * newNode = new Node; 
+            newNode->data = val; 
+
+            newNode->succ = prev->succ;
+            prev->succ = newNode;  
+        }
+
+        void insertInQueue(T val){
+            Node * newNode = new Node;
+            newNode->data = val; 
+            newNode->succ = nullptr; 
+
+            Node * iter = this->head; 
+            if(iter == nullptr){
+                this->head = newNode; 
+            }else{
+                while(iter->succ!=nullptr){
+                    iter = iter->succ; 
+                }
+                iter->succ = newNode; 
+            }
+        }
+};
+
+int main(){
+    
+}
