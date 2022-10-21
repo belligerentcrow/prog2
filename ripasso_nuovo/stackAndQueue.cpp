390a8fab6d0ab5106b86444545768ab9eef3bb93
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2022-10-21 20:17:04 +0200

    stack and queue is working :thumbsup:

--- ripasso_nuovo/stackAndQueue.cpp
+++ ripasso_nuovo/stackAndQueue.cpp
@@ -49,21 +49,77 @@ class Stack{
 template <typename T>
 class Queue{
     private:
-        int head; 
-        int tail; 
+        int head=0; 
+        int tail=0; 
+        int n; 
+        T * queueArray; 
     public: 
-        Queue(); 
-        T enqueue(); 
-        T dequeue(); 
-        bool isEmpty(); 
+        Queue(int len): n(len){
+            queueArray = new T[len]; 
+        }; 
+        void enqueue(T x){
+            queueArray[this->tail] = x; 
+            if(this->tail == n){
+                this->tail = 0;
+            }else{
+                this->tail++; 
+            }
+        }; 
+        T dequeue(){
+            T x = queueArray[head]; 
+            if(this->head == n){
+                this->head = 0; 
+            }else{
+                this->head++; 
+            }
+            return x; 
+        }; 
+        bool isEmpty(){
+            if(head==tail){
+                return true; 
+            }else{
+                return false; 
+            }
+        }; 
+        bool isFull(){
+            if(this->head == this->tail+1){
+                return true; 
+            }else{
+                return false; 
+            }
+        }
+        void printQueue(){
+            cout <<"\t  |"<<endl<<"\t  |"<<endl<<"\t  V"<<endl<<"\t"; 
+            int j = head; 
+            while(j!= tail-1){
+                cout <<"["<< queueArray[j] <<"] <-- "; 
+                if(j == n){
+                    j = 0; 
+                }
+                j++; 
+            }
+            cout << "[" << queueArray[j]<<"] "; 
+        }
 };
 
 
 
 int main(){
     Stack<int> myStack(30);
+    Queue<int> myQueue(30); 
 
+    myQueue.enqueue(10); 
+    myQueue.enqueue(13);
+    myQueue.enqueue(39);
+    myQueue.enqueue(21);
+    myQueue.enqueue(124);
+    myQueue.dequeue(); 
+    myQueue.dequeue();
+    myQueue.dequeue(); 
+    myQueue.enqueue(420);
+    myQueue.enqueue(3294);
+    myQueue.enqueue(3248); 
+    myQueue.printQueue(); 
 
-
-    myStack.print();    
+    
 }
